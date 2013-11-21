c3f3f3045ed8c077df4b8207888571fbc2255b6e
benjaminadams <armastevs@gmail.com>
2013-11-21 12:25:20 -0600

    cleaned up config file

--- public/js/app/config/config.js
+++ public/js/app/config/config.js
@@ -1,12 +1,9 @@
 require.config({
     baseUrl: "./js/app",
-    // 3rd party script alias names (Easier to type "jquery" than "libs/jquery, etc")
-    // probably a good idea to keep version numbers in the file names for updates checking
     paths: {
         // Core Libraries
         "jquery": "../lib/jquery",
         'underscore': '../lib/underscore',
-        'underscore-string': '../lib/underscore-string',
         "backbone": "../lib/backbone",
         //"marionette": "../lib/marionette",
         "marionette": "../lib/marionettev1.2.3",
@@ -21,26 +18,18 @@ require.config({
         'localstorage': '../lib/jquery.total-storage',
         'moment': '../lib/moment',
         'console': '../lib/console',
-        // Plugins
         "backbone.validateAll": "../lib/plugins/Backbone.validateAll",
         "bootstrap": "../lib/plugins/bootstrap",
-        "text": "../lib/plugins/text",
+        "text": "../lib/plugins/text"
         //"wookmark": "../lib/jquery.wookmark",
         //   "imagesLoaded": "../lib/imagesLoaded",
-        "jasminejquery": "../lib/plugins/jasmine-jquery"
+        //"jasminejquery": "../lib/plugins/jasmine-jquery"
     },
     // Sets the configuration for your third party scripts that are not AMD compatible
     shim: {
         'underscore': {
             exports: '_'
         },
-        // Twitter Bootstrap jQuery plugins
-        "bootstrap": ["jquery"],
-        // jQueryUI
-        // "jqueryui": ["jquery"],
-        // jQuery mobile
-        //"jquerymobile": ["jqueryui"],
-
         // Backbone
         "backbone": {
             // Depends on underscore/lodash and jQuery
@@ -48,12 +37,6 @@ require.config({
             // Exports the global window.Backbone object
             "exports": "Backbone"
         },
-        'underscore-string': {
-            deps: [
-                'underscore'
-            ]
-        },
-
         //Marionette
         "marionette": {
             "deps": ["underscore", "backbone", "jquery"],
@@ -67,27 +50,17 @@ require.config({
         "handlebars": {
             "exports": "Handlebars"
         },
-        // Backbone.validateAll plugin that depends on Backbone
-        //"backbone.validateAll": ["backbone"],
-
-        "jasmine": {
-            // Exports the global 'window.jasmine' object
-            "exports": "jasmine"
-        },
-        // "bootbox": {
-        //     // Depends on underscore/lodash and jQuery
-        //     "deps": ["underscore", "jquery"],
-        //     // Exports the global window.Backbone object
-        //     "exports": "Bootbox"
-        // },
-
-        "jasmine-html": {
-            "deps": ["jasmine"],
-            "exports": "jasmine"
-        },
         "cookie": ["jquery"],
         "localstorage": ["jquery"]
-        // "isotope": ["jquery"]
+        //"jasmine": {
+        // Exports the global 'window.jasmine' object
+        //"exports": "jasmine"
+        //},
+        //"jasmine-html": {
+        // "deps": ["jasmine"],
+        //"exports": "jasmine"
+        //},
+
     },
     // hbs config - must duplicate in Gruntfile.js Require build
     hbs: {
