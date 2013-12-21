eb2322be753c76c18ed2edf763d5c0f9f57afc08
Ben <armastevs@gmail.com>
2013-12-20 20:46:27 -0600

    was linking articles to self post pages

--- public/js/app/view/post-row-view.js
+++ public/js/app/view/post-row-view.js
@@ -43,8 +43,8 @@ define(['App', 'jquery', 'underscore', 'backbone', 'view/basem-view', 'hbs!templ
                     this.model.set('external', 'data-bypass')
                 } else {
                     //so when the user returns to the subreddit page, we set the model back to non-external link
-                    this.model.set('url', this.model.get('permalink'))
-                    this.model.set('external', '')
+                    //  this.model.set('url', this.model.get('permalink'))
+                    //  this.model.set('external', '')
                 }
 
                 if (typeof data.expand !== 'undefined' && (this.model.get('embededImg') === true || this.model.get('selftext_html'))) {
