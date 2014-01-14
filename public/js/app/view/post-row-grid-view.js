3b459852bb3f67ab2c4006703d723ba45e3a1905
Ben <armastevs@gmail.com>
2014-01-14 17:52:17 -0600

    readded the ability to hover over imgs to load

--- public/js/app/view/post-row-grid-view.js
+++ public/js/app/view/post-row-grid-view.js
@@ -19,14 +19,13 @@ define(['App', 'jquery', 'underscore', 'backbone', 'view/basem-view', 'hbs!templ
                 this.attempts = 0 //how many times we attempt to render the view
                 if (this.biggerImg) { //don't preload/check for loading if the grid block does not have an img
 
-                    if (this.detectIfCached(this.smallerImg)) {
+                    if (this.detectIfCached(this.smallerImg) || this.detectIfCached(this.biggerImg)) {
                         this.allowedToRender = true
                     } else { //so dont try and preload self text posts or links
                         this.allowedToRender = false //so we only load x number of images at the same time
                         this.preloadImg()
                     }
                 }
-                //this.once("mouseenter", this.loadBiggerImg, this)
 
             },
             render: function() {
@@ -39,6 +38,33 @@ define(['App', 'jquery', 'underscore', 'backbone', 'view/basem-view', 'hbs!templ
                 var newPost = $(PostRowGridTmpl({
                     model: this.model.attributes
                 }))
+
+                if (this.biggerImg.split('.').pop() == 'gif') {
+                    newPost.find('.gridLoading').show()
+                    //newPost.find('.gridLoading').show() //only show loading icon if its a gif
+                }
+
+                if (this.smallerImg !== false) { //only need to hover over img when we have bigger img available
+                    newPost.one("mouseenter", function() {
+                        console.log("Loading bigger IMG");
+
+                        if (self.biggerImg.split('.').pop() == 'gif') {
+                            newPost.find('.gridLoading').attr('src', '/img/loading.gif')
+                            //newPost.find('.gridLoading').show() //only show loading icon if its a gif
+                        }
+
+                        $('<img src="' + self.biggerImg + '" />').load(function() {
+                            console.log('loaded img')
+                            newPost.find('img').attr('src', self.biggerImg);
+                            newPost.find('.gridLoading').hide() //hide loading gif
+                        }).error(function() {
+                            console.log("ERROR loading img")
+                            newPost.find('.gridLoading').hide() //hide loading gif
+                            //TODO show a failed to load img
+                        });
+
+                    });
+                }
 
                 //sometimes the columns are not setup yet, wait until they are
                 //TODO: fix this, bad practice
@@ -72,9 +98,10 @@ define(['App', 'jquery', 'underscore', 'backbone', 'view/basem-view', 'hbs!templ
             },
             preloadImg: function() {
                 var self = this
+                var imgToPreload = this.smallerImg || this.biggerImg
                 if (App.gridImagesLoadingCount < 10 && this.allowedToRender === false) {
                     App.gridImagesLoadingCount++;
-                    this.imgLoad = $('<img />').attr('src', this.smallerImg).load(function(data) {
+                    this.imgLoad = $('<img />').attr('src', imgToPreload).load(function(data) {
                         self.allowedToRender = true
                         self.render()
                         App.gridImagesLoadingCount--;
@@ -87,6 +114,8 @@ define(['App', 'jquery', 'underscore', 'backbone', 'view/basem-view', 'hbs!templ
                     App.once("gridView:imageLoaded", this.preloadImg, this);
                 }
             },
+
+            //when the user hovers over a grid block image load the gif/bigger img from imgur
             loadBiggerImg: function() {
                 var self = this
 
