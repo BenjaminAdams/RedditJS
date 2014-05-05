ccd23839453bf73006bcf31b767619ebb49d6466
Benjamin Adams <armastevs@gmail.com>
2014-05-05 13:56:52 -0500

    typo in server.js

--- server.js
+++ server.js
@@ -1,1 +1,1 @@
-require('server/server'); //azure needed a server.js file in the root of the directory.  TODO: Delete this later
+require('./server/server'); //azure needed a server.js file in the root of the directory.  TODO: Delete this later
