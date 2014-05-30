65507f006667b9cacf0b51e1fabd560c2b08fc93
BenjaminAdams <armastevs@gmail.com>
2014-05-29 22:14:24 -0500

    live MD parser needed more room at bottom

--- public/js/app/view/redirect.js
+++ public/js/app/view/redirect.js
@@ -9,7 +9,6 @@ define(['App', 'jquery', 'underscore', 'backbone', 'view/basem-view', 'hbs!templ
 			onRender: function() {
 				//delete users subreddits so we can fetch again
 				_.invoke(App.subreddits.mine.toArray(), 'destroy');
-				//query the api for /me.json
 				$.totalStorage('subreddits', null);
 
 				window.close()
