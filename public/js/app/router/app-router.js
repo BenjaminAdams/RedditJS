f31c37abf5bc2908447b5f58840143f6affe1a00
benjaminadams <armastevs@gmail.com>
2014-06-11 17:06:53 -0500

    can use subreddit css everywhere

--- public/js/app/router/app-router.js
+++ public/js/app/router/app-router.js
@@ -376,11 +376,11 @@ define(['App', 'underscore', 'backbone', 'marionette', 'view/header-view', 'view
             },
 
             loadSettingsFromCookies: function() {
-                var checkboxes = new Array("btmbar", "cmtLoad", "customCSS", "showSidebar", "enableNightmode", "infin", 'hideSelf');
+                var checkboxes = new Array("btmbar", "cmtLoad", "showSidebar", "infin", 'hideSelf');
                 var selectboxes = new Array('linkCount')
 
                 for (var i in checkboxes) {
-                    if ((typeof $.cookie(checkboxes[i]) === 'undefined' && checkboxes[i] !== 'enableNightmode') || $.cookie(checkboxes[i]) == 'true') {
+                    if ((typeof $.cookie(checkboxes[i]) === 'undefined') || $.cookie(checkboxes[i]) == 'true') {
                         App.settings.set(checkboxes[i], true)
 
                     } else {
@@ -396,8 +396,13 @@ define(['App', 'underscore', 'backbone', 'marionette', 'view/header-view', 'view
                     }
                 }
 
+                //update cssType 
+                App.settings.set('cssType', $.cookie('cssType') || 'useSrStyles')
+                //update useSrEverywhereTxt
+                App.settings.set('useSrEverywhereTxt', $.cookie('useSrEverywhereTxt') || '')
+
                 //load grid opt from cookies
-                App.settings.set('gridOption', $.cookie('gridOption'))
+                App.settings.set('gridOption', $.cookie('gridOption') || '')
 
             },
             checkIfNightmode: function() {
@@ -410,8 +415,10 @@ define(['App', 'underscore', 'backbone', 'marionette', 'view/header-view', 'view
                     App.settings.set('enableNightmode', false)
                 }
 
-                if (App.settings.get('enableNightmode') === true && $("#subredditStyle").attr('href') !== 'css/dark/styles.min.css') {
+                if (App.settings.get('cssType') === 'nightmode' && $("#subredditStyle").attr('href') !== 'css/dark/styles.min.css') {
                     $("#subredditStyle").attr("href", "css/dark/styles.min.css");
+                } else if (App.settings.get('cssType') === 'useSrEverywhere') {
+                    $("#subredditStyle").attr("href", "https://pay.reddit.com/r/" + App.settings.get('useSrEverywhereTxt') + "/stylesheet");
                 }
             }
 
