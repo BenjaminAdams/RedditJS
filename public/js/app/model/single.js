afe28e52da6002458710d35ae1bb7babc14bfd9f
BenjaminAdams <armastevs@gmail.com>
2014-08-06 00:20:51 -0500

    had double slash in url again

--- public/js/app/model/single.js
+++ public/js/app/model/single.js
@@ -17,7 +17,7 @@ define(['App', 'underscore', 'backbone', 'collection/comments', 'model/base'], f
 				return "/api/?url=comments/" + this.id + ".json" + sortOrderStr
 			} else {
 				//use jsonp if user is not logged in
-				return App.baseURL + "/comments/" + this.id + ".json?jsonp=?" + sortOrderStr
+				return App.baseURL + "comments/" + this.id + ".json?jsonp=?" + sortOrderStr
 			}
 		},
 		// Default attributes 
