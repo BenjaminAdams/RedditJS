19cc659711bf0be5ee599edc0c7100229e17ca40
BenjaminAdams <armastevs@gmail.com>
2014-08-05 23:24:35 -0500

    bots dont need to load bottom bar

--- public/js/app/App.js
+++ public/js/app/App.js
@@ -11,6 +11,8 @@ define(['jquery', 'backbone', 'marionette', 'underscore'],
             var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
             if (hash === "reqAsBot") {
                 App.baseURL = '/api/'
+
+                App.settings.set('btmbar', false)
             }
 
         }
