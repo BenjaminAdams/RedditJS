b99d70e77ee88f1dff2fd7dcec50ba1608a0861c
BenjaminAdams <armastevs@gmail.com>
2014-08-05 23:36:43 -0500

    bots on client side

--- public/js/app/App.js
+++ public/js/app/App.js
@@ -10,7 +10,9 @@ define(['jquery', 'backbone', 'marionette', 'underscore'],
         if (window.location.hash) {
             var hash = window.location.hash.substring(1); //Puts hash in variable, and removes the # character
             if (hash === "reqAsBot") {
-                App.baseURL = '/api/'
+                var fetchBots = ['http://robot85.herokuapp.com/', 'http://robot86.herokuapp.com/', 'http://robot87.herokuapp.com/']
+                var bot = fetchBots[Math.floor((Math.random() * fetchBots.length))]
+                App.baseURL = bot + '/api/'
 
                 //App.settings.set('btmbar', false)
             }
