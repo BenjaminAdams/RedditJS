4e2bd5b8d34879e3ae5590601888d511ab2330c2
BenjaminAdams <armastevs@gmail.com>
2014-09-09 00:21:52 -0500

    start of feature to click widget box to expand

--- public/post.js
+++ public/post.js
@@ -14,6 +14,7 @@
 		var script;
 		var cssTheme;
 		var showSubmit;
+		var borderColor;
 
 		(function init() {
 
@@ -25,6 +26,12 @@
 				postFinder = script.getAttribute('data-post-finder') || 'mostComments'
 				cssTheme = script.getAttribute('data-theme') || 'light'
 				showSubmit = script.getAttribute('data-show-submit') || 'true'
+
+				if (cssTheme === 'dark') {
+					borderColor = '#460000'
+				} else {
+					borderColor = '#5f99cf'
+				}
 
 				var embedUrl = "http://redditjs.com/embed?url=" + postUrl + '&width=' + width + '&height=' + height + '&postFinder=' + postFinder + '&cssTheme=' + cssTheme + '&showSubmit=' + showSubmit
 
@@ -45,8 +52,27 @@
 
 				setupMessenger(ifrm)
 
+				setupMaximizeBtn(ifrm)
+
 			}
 		})()
+
+		function setupMaximizeBtn(ifrm) {
+			//add button to top right of widget
+			var btn = document.createElement("div");
+			btn.style.height = '3%'
+			btn.style.width = '3%'
+			btn.style.margin = '0 auto'
+			btn.style.position = 'fixed'
+			btn.style.right = '19%'
+			btn.style.top = '0%'
+			btn.style.cursor = 'pointer'
+			btn.style.color = borderColor
+			//btn.className = 'fa fa-angellist'
+			btn.innerHTML = "&#11028;"
+			ifrm.parentNode.insertBefore(btn)
+
+		}
 
 		function setupMessenger(ifrm) {
 			var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
@@ -91,12 +117,6 @@
 
 		function addIframeCss(ifrm) {
 
-			var borderColor = '';
-			if (cssTheme === 'dark') {
-				borderColor = '#460000'
-			} else {
-				borderColor = '#5f99cf'
-			}
 			ifrm.style.border = '2px ' + borderColor + ' solid'
 			ifrm.style.resize = 'both';
 			ifrm.style.overflow = 'auto';
