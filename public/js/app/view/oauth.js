7a8c202d736e406308c5061cf4bb644b491a872d
Benjamin Adams <armastevs@gmail.com>
2014-12-21 18:43:17 -0600

    changed login window to be wide enough for new reddit login page layout

--- public/js/app/view/oauth.js
+++ public/js/app/view/oauth.js
@@ -1,46 +1,46 @@
 define(['App', 'jquery', 'underscore', 'backbone', 'hbs!template/oauth', 'view/login-view', 'model/user-about', 'cookie'],
-	function(App, $, _, Backbone, Tmpl, LoginView, UserModel, cookie) {
-		return LoginView.extend({
-			template: Tmpl,
-			events: {
-				'submit #login_reg': 'register',
-				'click .capimage': 'getNewCaptcha',
-				'click .hidecover': 'hide',
-				'submit #login_login': 'doLogin'
-			},
-			initialize: function(data) {
-				_.bindAll(this);
+  function(App, $, _, Backbone, Tmpl, LoginView, UserModel, cookie) {
+    return LoginView.extend({
+      template: Tmpl,
+      events: {
+        'submit #login_reg': 'register',
+        'click .capimage': 'getNewCaptcha',
+        'click .hidecover': 'hide',
+        'submit #login_login': 'doLogin'
+      },
+      initialize: function(data) {
+        _.bindAll(this);
 
-				App.on("login", this.loginSuccess, this);
-			},
-			onRender: function() {
-				console.log('rendering oauth popup')
-			},
+        App.on("login", this.loginSuccess, this);
+      },
+      onRender: function() {
+        //console.log('rendering oauth popup')
+      },
 
-			doLogin: function(e) {
-				e.preventDefault()
-				e.stopPropagation()
-				this.login()
+      doLogin: function(e) {
+        e.preventDefault()
+        e.stopPropagation()
+        this.login()
 
-			},
-			hide: function(e) {
-				e.preventDefault()
-				e.stopPropagation()
+      },
+      hide: function(e) {
+        e.preventDefault()
+        e.stopPropagation()
 
-				this.$el.empty()
-			},
+        this.$el.empty()
+      },
 
-			loginSuccess: function() {
-				var self = this
-				this.$el.empty()
-				var curHash = Backbone.history.fragment
-				Backbone.history.navigate('redirectAfterLogin'); //have to redirect to a fake link before we goback to where the user wants to go
-				Backbone.history.navigate(curHash, {
-					trigger: true
+      loginSuccess: function() {
+        var self = this
+        this.$el.empty()
+        var curHash = Backbone.history.fragment
+        Backbone.history.navigate('redirectAfterLogin'); //have to redirect to a fake link before we goback to where the user wants to go
+        Backbone.history.navigate(curHash, {
+          trigger: true
 
-				})
+        })
 
-			}
+      }
 
-		});
-	});
+    });
+  });
