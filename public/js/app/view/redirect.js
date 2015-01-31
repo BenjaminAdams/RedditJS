8a3ff62b74dff5f243085aae4491b6c012ad7910
Benjamin Adams <armastevs@gmail.com>
2015-01-31 10:23:10 -0600

    fixed a bug caused by the marionette upgrade tool

--- public/js/app/view/redirect.js
+++ public/js/app/view/redirect.js
@@ -1,19 +1,19 @@
 define(['App', 'jquery', 'underscore', 'backbone', 'view/basem-view', 'hbs!template/blank', 'cookie'],
-	function(App, $, _, Backbone, BaseView, Tmpl, Cookie) {
-		return BaseView.extend({
-			template: Tmpl,
+  function(App, $, _, Backbone, BaseView, Tmpl, Cookie) {
+    return BaseView.extend({
+      template: Tmpl,
 
-			initialize: function(data) {
-				_.bindAll(this);
-			},
-			onRender: function() {
-				//delete users subreddits so we can fetch again
-				_.invoke(App.subreddits.mine.toArray(), 'destroy');
-				$.totalStorage('subreddits', null);
+      initialize: function(data) {
+        _.bindAll(this);
+      },
+      onRender: function() {
+        //delete users subreddits so we can fetch again
+        _.invoke(App.subreddits.mine.toArray(), 'destroy');
+        $.totalStorage('subreddits', null);
 
-				window.destroy()
-			}
+        window.close()
+      }
 
-		});
+    });
 
-	});
+  });
