da6bc4c1d277357c0f218495a89158a0aead3194
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-26 05:04:18 +0800

    Delete public directory

--- public/js/app/controllers/Controller.js
+++
@@ -1,9 +0,0 @@
-define(['App', 'backbone', 'marionette'],
-    function(App, Backbone, Marionette) {
-        return Backbone.Marionette.Controller.extend({
-            initialize: function(options) {
-
-            }
-
-        });
-    });
--- public/js/app/template/helpers/yeller.js
+++
@@ -1,9 +0,0 @@
-define(['handlebars'], function(Handlebars) {
-    function yeller(context, options) {
-        // Assume it's a string for simplicity.
-        return context + "!!!!!!!!";
-    }
-
-    Handlebars.registerHelper('yeller', yeller);
-    return yeller;
-});
